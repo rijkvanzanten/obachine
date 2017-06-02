f79863288a86eaea8e615169871ca49834a0b9a5
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-06-02 12:54:54 +0200

    add webpack

---
+++ app/index.js
@@ -0,0 +1,4 @@
+import component from './component';
+import './main.css';
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
+  },
+  parts.lintJavascript({ include: PATHS.app }),
+  parts.lintCSS({ include: PATHS.app }),
+]);
+
+const productionConfig = merge([
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
+]);
+
+const developmentConfig = merge([
+  parts.loadCSS(),
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = (env) => {
+  if (env.target === 'production') {
+    return merge(commonConfig, productionConfig);
+  }
+
+  return merge(commonConfig, developmentConfig);
+};
