0a0146b83a8b0369d7f42b537dd0a1cf370df94b
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-02 14:20:06 +0200

    ✨ Setup Webpack Config (#1)
    * add webpack
    
    * :wrench: ignore build folder
    
    * :wrench: remove xo in favor of webpack based solution
    
    * :wrench: replace eslint with xo
    
    * :wrench: disable import/no-unresolved error
    
    this fixes the webpack resolve error for now
    
    * :rotating_light: lint webpack config files
    
    * :package: update package-lock
    
    * :wrench: replace postcss-stylelint combo with stylelint-webpack-plugin
    
    this provides us with nicer errors in the console
    
    * :fire: remove build files from git
    
    * :memo: add start command to readme
    
    * :art: fix example

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# oba
+# Obachine
+
+## Usage
+`npm start` starts app in development (HMR) mode
+`npm run build` builds app in production mode
---
+++ webpack.parts.js
@@ -0,0 +1,97 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+exports.devServer = ({host, port} = {}) => ({
+  devServer: {
+    historyApiFallback: true,
+    stats: 'errors-only',
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    overlay: {
+      errors: true,
+      warnings: true
+    }
+  }
+});
+
+exports.lintJavascript = ({include, exclude, options}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        enforce: 'pre',
+
+        loader: 'xo-loader',
+        options
+      }
+    ]
+  }
+});
+
+exports.loadCSS = ({include, exclude} = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+});
+
+exports.extractCSS = ({include, exclude, use}) => {
+  // Output extracted to CSS to a file
+  const plugin = new ExtractTextPlugin({
+    filename: '[name].css'
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: plugin.extract({
+            use,
+            fallback: 'style-loader'
+          })
+        }
+      ]
+    },
+    plugins: [plugin]
+  };
+};
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => ([
+      require('autoprefixer')()
+    ])
+  }
+});
+
+exports.lintCSS = ({include, exclude}) => {
+  const plugin = new StyleLintPlugin({
+    files: ['app/**/*.css']
+  });
+
+  return {
+    plugins: [plugin]
+  }
+};
