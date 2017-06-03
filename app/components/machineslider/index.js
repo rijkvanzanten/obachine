d9a731e774033325e6ad4fab544141c0658f502a
Pierre Bleeker <pierrebleeker@Pierres-MacBook-Pro.local>
2017-06-03 14:22:47 +0200

    :hankey: Add next/prev to slider

--- app/components/machineslider/index.js
+++ app/components/machineslider/index.js
@@ -5,8 +5,12 @@ import styles from './machineslider.css';
 export default () => // Create html template
 html`
   <div class=${styles.machineslider}>
-    <button class=${styles.machineslider.prev}>◀</button>
-    <div>Welke genres hou je van?</div>
-    <button class=${styles.machineslider.next}>▶</button>
+    <button class=${styles.prev} onclick="next()">◀</button>
+      <div class=${styles.buildingblock}>Welke genres hou je van?</div>
+      <div class=${styles.buildingblock}>Welke type hou je van?</div>
+      <div class=${styles.buildingblock}>Welke kleur hou je van?</div>
+    <button class=${styles.next} onclick="console.log('joe')">▶</button>
   </div>
 `;
+
+// Cnsole.log(blocks.length);
