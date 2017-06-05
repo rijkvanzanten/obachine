6b14bc6f9a2f2d0bcc1ae669cd41e9b398955439
Pierre Bleeker <pierrebleeker@Pierres-MacBook-Pro.local>
2017-06-05 15:24:53 +0200

    :lipstick: Add machine-blocks

--- app/components/machine/index.js
+++ app/components/machine/index.js
@@ -3,13 +3,15 @@ import styles from './machine.css';
 // Import block from './machine-1.svg';
 
 const svgs = {
-  genre: require('./machine-1.svg'),
-  type: require('./machine-1.svg'),
-  year: require('./machine-1.svg'),
-  place: require('./machine-1.svg'),
-  author: require('./machine-1.svg'),
-  pages: require('./machine-1.svg'),
-  color: require('./machine-1.svg')
+  core: require('./machine-core.svg'),
+  type: require('./machine-genre.svg'),
+  author: require('./machine-author.svg'),
+  color: require('./machine-color.svg'),
+  start: require('./start-tunnel.svg'),
+  end: require('./end-tunnel.svg'),
+  piperl: require('./pipe-rl.svg'),
+  pipemiddle: require('./pipe-middle.svg'),
+  piper: require('./pipe-middle.svg')
 };
 
 console.log(svgs);
@@ -17,8 +19,16 @@ console.log(svgs);
 // Export module
 export default () => // Create html template
 html`
-  <div>
-    <h1 class=${styles.machine}>bowen</h1>
-    <img src=${svgs.genre} role="presentation" />
+  <div class=${styles.container}>
+    <h1 class=${styles}></h1>
+    <img class=${styles.tunnel} src=${svgs.start} role="presentation" />
+    <img class=${styles.machine} src=${svgs.core} role="presentation" />
+    <img class=${styles.pipem} src=${svgs.pipemiddle} role="presentation" />
+    <img class=${styles.machine} src=${svgs.color} role="presentation" />
+    <img class=${styles.piperl} src=${svgs.piperl} role="presentation" />
+    <img class=${styles.machine} src=${svgs.author} role="presentation" />
+    <img class=${styles.piper} src=${svgs.piper} role="presentation" />
+    <img class=${styles.machine} src=${svgs.type} role="presentation" />
+    <img class=${styles.tunnel} src=${svgs.end} role="presentation" />
   </div>
 `;
