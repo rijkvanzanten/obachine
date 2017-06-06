f8fd2aaf5dcb6e3e53bd5d61cae347e29203b988
pierman1 <pbleeker@live.nl>
2017-06-06 11:23:57 +0200

    :lipstick: Add button and button-styles (#8)

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,13 +1,14 @@
 import html from 'choo/html';
 import header from '../components/header';
 import footer from '../components/footer';
-import modal from '../components/modal';
+import button from '../components/button';
 import styles from './body.css';
 
 export default function main(state, emit) {
   return html`
     <body class=${styles.body}>
       ${header(state, emit)}
+      ${button()}      
       ${state.modal.active ? modal(state, emit) : null}
       ${footer()}
     </body>
