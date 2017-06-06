2112392afc50e31ed90258ddad6a3be50f28ae2c
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-06 15:23:26 +0200

    :sparkles: render parts with ids

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 import choo from 'choo';
+import shortid from 'shortid';
+import parts from './components/parts';
 
 import main from './routes/main';
 
@@ -11,38 +13,84 @@ app.route('/', main);
 app.mount('body');
 
 function setupState(state, emitter) {
-  state.machineslider = {
-    active: false,
-    items: ['genre', 'type', 'year', 'place', 'author', 'pages', 'color'],
-    current: 0
-  };
+  Object.assign(state, {
+    machineslider: {
+      active: false,
+      items: ['genre', 'type', 'year', 'place', 'author', 'pages', 'color'],
+      current: 0
+    },
+    /*
+     * {
+     *   [id]: {
+     *     type: String,
+     *     value: String,
+     *     order: Number
+     *   }
+     * }
+     */
+    machineparts: {},
+    modal: {
+      active: false,
+      content: {}
+    }
+  });
 
-  state.machineparts = [];
+  emitter.on('select-nextItem', onSelectNextItem);
+  emitter.on('select-prevItem', onSelectPrevItem);
+  emitter.on('select-add', onSelectAdd);
 
-  emitter.on('select-nextItem', () => {
+  emitter.on('showModal', showModal);
+  emitter.on('hideModal', hideModal);
+
+  emitter.on('updateValue', updateValue);
+
+  function onSelectNextItem() {
     state.machineslider.current++;
     if (state.machineslider.current >= state.machineslider.items.length) {
       state.machineslider.current = 0;
     }
 
     emitter.emit('render');
-  });
+  }
 
-  emitter.on('select-prevItem', () => {
+  function onSelectPrevItem() {
     state.machineslider.current--;
     if (state.machineslider.current < 0) {
       state.machineslider.current = state.machineslider.items.length;
     }
 
     emitter.emit('render');
-  });
+  }
 
-  emitter.on('select-add', item => {
-    state.machineparts.push({
-      type: item,
-      value: ''
+  function onSelectAdd(type) {
+    const id = shortid.generate();
+
+    state.machineparts[id] = {
+      type,
+      value: '',
+      order: Object.keys(state.machineparts).length
+    };
+
+    emitter.emit('showModal', {
+      id,
+      content: parts[type].modal
     });
+  }
+
+  function showModal(modalContent) {
+    state.modal.content = modalContent;
+    state.modal.active = true;
 
     emitter.emit('render');
-  });
+  }
+
+  function hideModal() {
+    state.modal.active = false;
+    emitter.emit('render');
+  }
+
+  function updateValue({id, value}) {
+    state.machineparts[id].value = value;
+    emitter.emit('render');
+  }
 }
--- app/routes/main.js
+++ app/routes/main.js
@@ -2,27 +2,40 @@ import html from 'choo/html';
 import parts from '../components/parts';
 import header from '../components/header';
 import machineslider from '../components/machineslider';
+import modal from '../components/settings-modal';
 import styles from './body.css';
 
 export default function main(state, emit) {
+  const {active} = state.modal;
+
   return html`
     <body class=${styles.body}>
       ${header(state, emit)}
       ${machineslider(state.machineslider, emit)}
       <form>
         <ul>
-          ${state.machineparts.map(machine)}
+          ${Object.keys(state.machineparts).map(machine)}
         </ul>
         <button type="submit">Machine Aanzwengelen</button>
       </form>
+      ${active ? modal(state.modal.content, emit) : null}
     </body>
   `;
 
-  function machine({type}) {
+  function machine(id) {
+    const type = state.machineparts[id].type;
+
+    function showModal() {
+      emit('showModal', {
+        id,
+        content: parts[type].modal
+      });
+    }
+
     return html`
-      <li>
+      <li data-id=${id} onclick=${showModal}>
         <input type="hidden" name=${type} />
-        ${parts[type]()}
+        ${parts[type].machine()}
       </li>
     `;
   }
