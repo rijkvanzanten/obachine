5a6d8835ec88a7e533a261e8933f85dde7245b6a
pierman1 <pbleeker@live.nl>
2017-06-06 11:28:53 +0200

    Feature/machine slider (#10)
    * :lipstick: Add slider styles
    
    * :hankey: Add next/prev to slider
    
    * :hankey: Nothing changed really
    
    * :bug: Made machine-slider work
    
    * :boom: Change array of machine-building-blocks into object
    
    * :art: Add svg to machine-building-block
    
    * :art: Add svg code to state
    
    * :sparkles: add svgs based on machine name (#7)
    
    * :lipstick: Machines
    
    * :lipstick: Add machine-blocks

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,6 +1,8 @@
 import html from 'choo/html';
 import header from '../components/header';
 import footer from '../components/footer';
+import machineslider from '../components/machineslider';
+import machine from '../components/machine';
 import button from '../components/button';
 import styles from './body.css';
 
@@ -8,6 +10,8 @@ export default function main(state, emit) {
   return html`
     <body class=${styles.body}>
       ${header(state, emit)}
+      ${machineslider(state, emit)}
+      ${machine()}
       ${button()}      
       ${state.modal.active ? modal(state, emit) : null}
       ${footer()}
