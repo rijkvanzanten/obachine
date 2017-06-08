8670fc96b04760ee7012430df5679633dcad5861
GiuliaM <Gekke_giulia@hotmail.com>
2017-06-08 13:19:55 +0200

    :sparkles: created a loop for the switches and started to rotate elements

--- animation/scripts/script.js
+++ animation/scripts/script.js
@@ -1,12 +1,13 @@
-const svg = Snap("#test");
+/* global Snap */
+const svg = Snap('#test');
 const bigCircle = svg.circle(150, 150, 100);
 
-const loop = function(r) {
+const loop = function (r) {
   const newR = r === 50 ? 100 : 50;
   bigCircle.animate({
-    r:r
+    r
   }, 1500,
-  function() {
+  () => {
     loop(newR);
   });
 };
@@ -15,71 +16,103 @@ loop(50);
 
 bigCircle.drag();
 
-//cogwheelMachine
-const cogwheelMachine = Snap("#cogwheelMachine");
-const greenSwitch = cogwheelMachine.select("#greenSwitch");
+// CogwheelMachine
+const cogwheelMachine = Snap('#cogwheelMachine');
+const greenSwitch = cogwheelMachine.select('#greenSwitch');
 
-const switchLoop = function(cx) {
+const switchLoop = function (cx) {
   const newPosition = cx === 55 ? 38 : 55;
 
   greenSwitch.animate({
-      cx:cx
+    cx
   }, 1000,
-  function() {
+  () => {
     switchLoop(newPosition);
   });
 };
 
 switchLoop(38);
 
-const redSwitch = cogwheelMachine.select("#redSwitch");
+const redSwitch = cogwheelMachine.select('#redSwitch');
 redSwitch.animate({
-    cx: 55
+  cx: 55
 }, 1000);
 
-const firstControl = cogwheelMachine.select("#firstControl");
+const firstControl = cogwheelMachine.select('#firstControl');
 firstControl.animate({
-    cy: 5
+  cy: 5
 }, 1000);
 
-const thirdControl = cogwheelMachine.select("#thirdControl");
+const thirdControl = cogwheelMachine.select('#thirdControl');
 thirdControl.animate({
-    cy: 20
+  cy: 20
 }, 1000);
 
-const topCogwheel = cogwheelMachine.select("#topCogwheel");
+const topCogwheel = cogwheelMachine.select('#topCogwheel');
+// topCogwheel.attr({
+//   cx: 0,
+//   cy: 0
+// });
 
-// topCogwheel.translate(0, 18);
-// topCogwheel.transform(topCog);
+const looping = function looping() {
+  const bbox = topCogwheel.getBBox();
+  topCogwheel.stop().animate({
+    transform: 'r25'
+  }, 1000,
+  () => {
+    looping(bbox);
+  });
+};
 
-topCogwheel.animate({
-  transform: "r0 100 100"
-  }, 1000);
+looping();
+//
+// function rotating() {
+//   const bbox = topCogwheel.getBBox(); // Bounding box, get coords and centre
+//
+//   topCogwheel.animate({
+//     transform: 'r10,' + bbox.cx + ',' + bbox.cy
+//   }, 1000);
+// };
+//
+// rotating();
 
 cogwheelMachine.drag();
 
+// First robot
+const displayMachine = Snap('#displayMachine');
+// const button = displayMachine.select('#button');
+const upperbar = displayMachine.select('#upperbar');
 
+// const turn = function () {
+//   const newT = r === 50 ? 100 : 50;
+//   bigCircle.animate({
+//     r
+//   }, 1500,
+//   () => {
+//     loop(newR);
+//   });
+// }
+// button.rotate(0);
+// button.rotate(360);
+//
+// eve.on('snap.rotated.*', function() {
+//     console.log(this, 'was rotated');
+// });
 
-//First robot
-const displayMachine = Snap("#displayMachine");
-const button = displayMachine.select("#button");
-const upperbar = displayMachine.select("#upperbar");
-
-
-const blink = function(fill) {
-  const newColor= fill === ("#f00") ? ("#fb0") : ("#f00");
+const blink = function (fill) {
+  const newColor = fill === ('#f00') ? ('#fb0') : ('#f00');
 
   upperbar.animate({
-      fill:fill
+    fill
   }, 1000,
-  function() {
+  () => {
     blink(newColor);
   });
 };
 
 blink();
 
-// upperbar.attr({
+// Upperbar.attr({
 //   fill: "red"
 // });
 
@@ -89,11 +122,34 @@ blink();
 //   }, 1000);
 //
 
-//Make the SVG dragable
+// Make the SVG dragable
 displayMachine.drag();
 
-//Second robot
-const secondRobot = Snap("#bookRobot2");
 
-//Make the SVG dragable
-// secondRobot.drag();
+
+// Snippets
+// TopCogwheel.translate(0, 18);
+// topCogwheel.transform(topCog);
+// topCogwheel.attr({
+//   cx: 123,
+//   cy: 60,
+// });
+
+// topCogwheel.transform('r10');
+
+// topCogwheel.animate({
+//   transform: ('r360')
+// }, 5000);
+
+// const cogLoop = function (cx) {
+//   const newTurn = cx === 55 ? 38 : 55;
+//
+//   greenSwitch.animate({
+//     cx
+//   }, 1000,
+//   () => {
+//     switchLoop(newPosition);
+//   });
+// };
+//
+// switchLoop(38);
