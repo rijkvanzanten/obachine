b02d268236846308ab2d7dc04d5661583d6b8188
pierman1 <pbleeker@live.nl>
2017-06-08 17:05:12 +0200

    :fire: remove unused files

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ function setupState(state, emitter) {
   Object.assign(state, {
     machineslider: {
       active: false,
-      items: ['genre', 'auteur', 'type', 'place', 'pages', 'color'],
+      items: ['genre', 'auteur', 'type', 'plaats', 'kleur'],
       current: 0
     },
     machineparts: {
