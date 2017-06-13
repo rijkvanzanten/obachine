b8af48b5411ae8abf4f794389adcf97370d0a453
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-06-14 00:55:18 +0200

    Add/enhancements (#40)
    * :pencil2: rename machine submit
    
    * :lipstick: add text-decoration none to list-items on results
    
    * :lipstick: restructure results page
    
    * :art: add link to logo
    
    * :lipstick: add styles
    
    fix detail page, fix results page, fix header
    
    * :lipstick: 1.225rem to 1.125rem

--- app/components/settings-modal/index.js
+++ app/components/settings-modal/index.js
@@ -5,11 +5,13 @@ export default ({id, content, value}, emit) => {
   const {title, children, color} = content;
 
   return html`
-    <div class=${styles.modal} style="background-color: ${color}">
-      <h2 class="${styles.title}">${title}</h2>
-      <button class=${styles.close} onclick=${close}>✕</button>
-      ${children(id, value, emit)}
-      <button class=${styles.button} onclick=${close}>Opslaan</button>
+    <div class=${styles.overlay}>
+      <div class=${styles.modal} style="background-color: ${color}">
+        <h2 class="${styles.title}">${title}</h2>
+        <button class=${styles.close} onclick=${close}>✕</button>
+        ${children(id, value, emit)}
+        <button class=${styles.button} onclick=${close}>Opslaan</button>
+      </div>
     </div>
   `;
 
