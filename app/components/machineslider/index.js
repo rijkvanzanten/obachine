16195797211408e04fe0f48426484ca136956af0
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-06-14 17:41:54 +0200

    :sparkles: rebuild the slider after user test

--- app/components/machineslider/index.js
+++ app/components/machineslider/index.js
@@ -2,17 +2,22 @@ import html from 'choo/html';
 import styles from './machineslider.css';
 import prevButton from './prev.svg';
 import nextButton from './next.svg';
+import parts from '../parts';
 
 export default (state, emit) => {
   const currentItem = state.items[state.current];
   return html`
-    <div class=${styles.machineslider}>
+    <li class=${styles.machineslider}>
       <button class=${styles.prev} onclick=${prev}><img src=${prevButton} alt="vorige machine"/></button>
-      <button data-item=${currentItem} class=${styles.buildingblock} onclick=${select}>
-        ${currentItem}
-      </button>
+      <div class=${styles.placeholder}>
+        <p>Kies een machine...</p>
+        <button data-item=${currentItem} class=${styles.buildingblock} onclick=${select}>
+          ${parts[currentItem].machine()}
+        </button>
+        <p>${currentItem}</p>
+      </div>
       <button class=${styles.next} onclick=${next}><img src=${nextButton} alt="volgende machine"/></button>
-    </div>
+    </li>
   `;
 
   function next() {
