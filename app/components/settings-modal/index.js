d0289a40caed9b03004e07fae83b0fd801d232e9
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-19 12:02:01 +0200

    :fire: Remove close button in modal (#63)
    Closes #57

--- app/components/settings-modal/index.js
+++ app/components/settings-modal/index.js
@@ -8,7 +8,6 @@ export default ({id, content, value}, emit) => {
     <div class=${styles.overlay}>
       <div class=${styles.modal} style="background-color: ${color}">
         <h2 class="${styles.title}">${title}</h2>
-        <button class=${styles.close} onclick=${close}>✕</button>
         ${children(id, value, emit)}
         <button class=${styles.button} onclick=${close}>Opslaan</button>
       </div>
