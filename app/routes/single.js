743dbc8f5ce8a54766e94ba170c3c911ac5e5a6a
pierman1 <pbleeker@live.nl>
2017-06-21 11:34:14 +0200

    Feature/single results styles (#90)
    * :lipstick: Add images to slider
    
    * :art: Add the right svg's
    
    * :art: replaced machine sliders
    
    * :ok_hand: Removed console log
    
    * :art: Add page numbers (description) & styles
    
    * :lipstick: Add padding to modal
    
    * :lipstick: Got rid off hover
    
    * :lipstick: Styles single page and results
    
    * :lipstick: Add bullet arrows to single
    
    * :lipstick: Sizes results/ margin / paddings

--- app/routes/single.js
+++ app/routes/single.js
@@ -48,30 +48,31 @@ export default function main(state, emit) {
       <main>
         <a href="/">◀ terug</a>
         <section class=${singleStyles.header}>
-          <h2>${titles}</h2>
-          <img src=${coverimages[1]} />
+          <h2>${state.store[state.params.id].titles['short-title']}</h2>
+          <img class=${singleStyles.cover} src=${state.store[state.params.id].coverimages.coverimage[1]} />
         </section>
         <section class=${singleStyles.summary}>
           <h3>Korte beschrijving</h3>
           <p>${summaries}</p>
         </section>
-        <h3>Genres</h3>
-        <ul>${genres.map(genre => html`<li>${genre}</li>`)}</ul>
-        <h3>Fysieke beschrijving</h3>
-        <p>${physicalDescriptions}</p>
-        <h3>Beschikbare formaten</h3>
-        <ul>
-          ${formats.length > 0 ?
-            formats.map(format => html`<li>${format}</li>`) :
-            null
-          }
-        </ul>
-        <h3>Editie</h3>
-        <p>${editions}</p>
-        <h3>Uitgeverij</h3>
-        <p>${publishers} - ${publishersYear}</p>
-        <h3>ISBN</h3>
-        <p>${isbn}</p>
+        <section class=${singleStyles.section}>
+          <h3>Genres</h3>
+          <ul>${genres.map(genre => html`<li>${genre}</li>`)}</ul>
+          <h3>Fysieke beschrijving</h3>
+          <p>${state.store[state.params.id].description['physical-description']}</p>
+          <h3>Beschikbare formaten</h3>
+          <ul>
+            ${formats.length > 0 ?
+              formats.map(format => html`<li>${format}</li>`) :
+              null
+            }
+          </ul>
+          <h3>Editie</h3>
+          <h3>Uitgeverij</h3>
+          <p>${state.store[state.params.id].publication.publishers.publisher.$t} - ${state.store[state.params.id].publication.publishers.publisher.year}</p>
+          <h3>ISBN</h3>
+          <p>${state.store[state.params.id].identifiers['isbn-id']}</p>
+        </section>
       </main>
     </body>
   `;
