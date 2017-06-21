f5436000a92e069ae6cd687dc31480fce47ed6f5
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-21 14:01:55 +0200

    :art: Improve code structure

--- app/routes/single.js
+++ app/routes/single.js
@@ -1,45 +1,18 @@
 import html from 'choo/html';
 import header from '../components/header';
 import singleStyles from './single.css';
-
-function toArray(value) {
-  let array = [];
-
-  if (value) {
-    if (Array.isArray(value)) {
-      array = value;
-    } else {
-      array = [value];
-    }
-  }
-
-  return array;
-}
+import {toArray, getValue} from '../utils';
 
 export default function main(state, emit) {
-  const genres = toArray(state.store[state.params.id].genres.genre);
-  const formats = toArray(state.store[state.params.id].formats.format);
-
-  function getValue(object, ...keys) {
-    keys.forEach(key => {
-      if (object[key]) {
-        object = object[key];
-      } else {
-        console.log('Key ' + key + ' bestaat niet op object');
-        return false;
-      }
-    });
-
-    return object;
-  }
-
-  const titles = getValue(state, 'store', state.params.id, 'titles', 'short-title') ? getValue(state, 'store', state.params.id, 'titles', 'short-title') : 'Onbekend';
-  const coverimages = getValue(state, 'store', state.params.id, 'coverimages', 'coverimage') ? getValue(state, 'store', state.params.id, 'coverimages', 'coverimage') : 'Onbekend';
-  const summaries = getValue(state, 'store', state.params.id, 'summaries', 'summary') ? getValue(state, 'store', state.params.id, 'summaries', 'summary') : 'Onbekend';
-  const specifications = getValue(state, 'store', state.params.id, 'description', 'physical-description') ? getValue(state, 'store', state.params.id, 'description', 'physical-description') : 'Onbekend';
+  const genres = toArray(getValue(state, 'store', state.params.id, 'genres', 'genre'));
+  const formats = toArray(getValue(state, 'store', state.params.id, 'formats', 'format'));
+  const titles = getValue(state, 'store', state.params.id, 'titles', 'short-title');
+  const coverimages = getValue(state, 'store', state.params.id, 'coverimages', 'coverimage');
+  const summaries = getValue(state, 'store', state.params.id, 'summaries', 'summary');
+  const specifications = getValue(state, 'store', state.params.id, 'description', 'physical-description');
   const editions = getValue(state, 'store', state.params.id, 'publication', 'editions', 'edition');
-  const publishers = getValue(state, 'store', state.params.id, 'publication', 'publishers', 'publisher', '$t') ? getValue(state, 'store', state.params.id, 'publication', 'publishers', 'publisher', '$t') : 'Onbekend';
-  const publishersYear = getValue(state, 'store', state.params.id, 'publication', 'publishers', 'publisher', 'year') ? getValue(state, 'store', state.params.id, 'publication', 'publishers', 'publisher', 'year') : 'Onbekend';
+  const publishers = getValue(state, 'store', state.params.id, 'publication', 'publishers', 'publisher', '$t');
+  const publishersYear = getValue(state, 'store', state.params.id, 'publication', 'publishers', 'publisher', 'year');
   const isbn = getValue(state, 'store', state.params.id, 'identifiers', 'isbn-id');
 
   return html`
@@ -55,10 +28,14 @@ export default function main(state, emit) {
           <h3>Korte beschrijving</h3>
           <p>${summaries}</p>
         </section>
-        <h3>Genres</h3>
-        <ul>${genres.map(genre => html`<li>${genre}</li>`)}</ul>
-        <h3>Specificaties</h3>
-        <p>${specifications}</p>
+        ${genres.length > 0 ? html`
+          <h3>Genres</h3>
+          <ul>${genres.map(genre => html`<li>${genre}</li>`)}</ul>
+        ` : null}
+        ${specifications.length > 0 ? html`
+          <h3>Specificaties</h3>
+          <p>${specifications}</p>
+        ` : null}
         <h3>Beschikbare formaten</h3>
         <ul>
           ${formats.length > 0 ?
