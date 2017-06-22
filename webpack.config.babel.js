603c60b4a12b958a93777316fb4f1e12dd43b5a5
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-06-22 17:24:56 +0200

    :wrench: add favicon loading through webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const HappyPack = require('happypack');
 const parts = require('./webpack.parts');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -25,6 +26,26 @@ const commonConfig = merge([
     plugins: [
       new HtmlWebpackPlugin({
         template: 'app/obachine-index.html',
+      }),
+      new FaviconsWebpackPlugin({
+        logo: './app/favicon.png',
+        prefix: 'icons.[hash]/',
+        emitStats: false,
+        inject: true,
+        background: '#fff',
+        title: 'Obachine',
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
       }),
       new HappyPack({
         loaders: [
