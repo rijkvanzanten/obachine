4184b639ef7e2ab77b8ac16bfdc236f80f17a3d2
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-06-23 20:58:39 +0200

    :lipstick: remove logo class from logo img

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -4,6 +4,6 @@ import logo from './oba-logo.svg';
 
 export default () => html`
   <header class=${styles.header}>
-    <a href="/"><img class=${styles.logo} src="${logo}" alt="oba logo"></a>
+    <a href="/"><img src="${logo}" alt="oba logo"></a>
   </header>
 `;
