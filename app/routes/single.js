fb228ce05b4466c584ec724606f11c78acfa0127
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-06-23 19:58:14 +0200

    :lipstick: add styling

--- app/routes/single.js
+++ app/routes/single.js
@@ -37,7 +37,7 @@ export default function single(state, emit) {
     return html`
       <body class=${styles.body}>
         ${header(state, emit)}
-        <main>
+        <main class="${styles.main}">
           <a href="/">◀ terug</a>
           <section class=${styles.header}>
             <h2>${title}</h2>
@@ -47,21 +47,21 @@ export default function single(state, emit) {
             <h3>Korte beschrijving</h3>
             <p>${summaries}</p>
           </section>
-          <div class="${styles.availability}">${renderAvailability(availability)}</div>
-          <div class="${styles.reviews}">${renderReviews(reviews)}</div>
-          <div class="${styles.formats}">${formats.length > 0 ? renderList('Formats', formats) : null}</div>
-          <div class="${styles.genres}">${genres.length > 0 ? renderList('Genres', genres) : null}</div>
-          <div class="${styles.specifications}">${specifications.length > 0 ? renderList('Specifications', specifications) : null}</div>
+          ${renderAvailability(availability)}
+          ${renderReviews(reviews)}
+          ${formats.length > 0 ? renderList('Formats', formats) : null}
+          ${genres.length > 0 ? renderList('Genres', genres) : null}
+          ${specifications.length > 0 ? renderList('Specifications', specifications) : null}
         </main>
       </body>
     `;
 
     function renderList(title, values) {
       return html`
-        <div>
+        <section>
           <h3>${title}</h3>
           <ul>${values.map(value => renderLI(value))}</ul>
-        </div>
+        </section>
       `;
 
       function renderLI(value) {
@@ -72,13 +72,13 @@ export default function single(state, emit) {
     function renderAvailability(availability) {
       if (availability && availability.length > 0) {
         return html`
-          <div>
+          <section>
             <h3>Beschikbaarheid</h3>
             <details>
               <summary>Klik hier om de beschikbaarheid te bekijken</summary>
               <ul>${availability.map(({name, available}) => html`<li class="${styles.location}" data-available="${available}">${name}</li>`)}</ul>
             </details>
-          </div>
+          </section>
         `;
       }
 
@@ -88,7 +88,7 @@ export default function single(state, emit) {
     function renderReviews(reviews) {
       if (reviews && reviews.length > 0) {
         return html`
-          <div>
+          <section>
             <h3>Reviews <small>van Boekenliefde.nl</small></h3>
             <ul>
               ${reviews.map(({link, username, dateadded, ratingtitle, rating, review}) => html`
@@ -102,7 +102,7 @@ export default function single(state, emit) {
                 </li>
               `)}
             </ul>
-          </div>
+          </section>
         `;
       }
 
