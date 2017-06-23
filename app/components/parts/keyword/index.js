b207b0b2561c711ae6dc6aaca380d6b3775042ff
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-23 11:34:27 +0200

    :sparkles: Persist values in modals

--- app/components/parts/keyword/index.js
+++ app/components/parts/keyword/index.js
@@ -7,7 +7,7 @@ export const modalSettings = {
 
     return html`
         <form class=${styles.form} onsubmit=${preventSubmit} id=${id}>
-          <input type="text" name="keyword" oninput=${saveValues}>
+          <input type="text" name="keyword" oninput=${saveValues} value="${value || ''}>
         </form>
     `;
 
