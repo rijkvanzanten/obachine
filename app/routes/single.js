5ad795c7dd8f368fa097d994d050515c02880532
pierman1 <pbleeker@live.nl>
2017-06-27 18:02:14 +0200

    :lipstick: Change order single page, fix padding at reviews

--- app/routes/single.js
+++ app/routes/single.js
@@ -52,9 +52,9 @@ export default function single(state, emit) {
             <p>${summaries}</p>
           </section>
           ${renderAvailability(availability)}
-          ${renderReviews(reviews)}
           ${formats.length > 0 ? renderList('Formats', formats) : null}
           ${genres.length > 0 ? renderList('Genres', genres) : null}
+          ${renderReviews(reviews)}
         </main>
       </body>
     `;
